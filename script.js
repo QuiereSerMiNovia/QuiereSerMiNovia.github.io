bfb922f6e42acd3bf5eab8f8ee2f3d26566e0c7a
QuiereSerMiNovia <157184091+QuiereSerMiNovia@users.noreply.github.com>
2024-01-19 18:43:50 -0600

    innitial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function showMessage(message) {
+    alert(message);
+}
+
+function moveButton() {
+    const noBtn = document.getElementById('noBtn');
+    const newX = Math.random() * (window.innerWidth - noBtn.clientWidth);
+    const newY = Math.random() * (window.innerHeight - noBtn.clientHeight);
+    
+    noBtn.style.position = 'absolute';
+    noBtn.style.left = newX + 'px';
+    noBtn.style.top = newY + 'px';
+}
